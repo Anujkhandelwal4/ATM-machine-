17f484d8b98aad2bd14a03314fcef25b9dd5da37
Anujkhandelwal4 <65071584+Anujkhandelwal4@users.noreply.github.com>
2020-05-09 19:00:15 +0530

    Create ATM.java

---
+++ ATM.java
@@ -0,0 +1,11 @@
+package ATMmachine;
+import java.io.*;
+public class ATM extends OptionMenu {
+
+	public static void main(String[] args) throws IOException {
+		OptionMenu o=new OptionMenu();
+		o.getlogin();
+
+	}
+
+}
